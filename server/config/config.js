bb02ce38b0c1a741f02164db6afc73dda5a8583c
git <komrad.hlopenkoff>
2014-07-15 23:26:04 +0400

    please

--- server/config/config.js
+++ server/config/config.js
@@ -3,14 +3,15 @@ var rootPath = path.normalize(__dirname + '/../../');
 
 module.exports = {
   development: {
-    db: 'mongodb://localhost/xecutionerartsblog',
+//    db: 'mongodb://localhost/xecutionerartsblog',
+      db: 'mongodb://Xecutioner:rbcgnhvf1@ds053798.mongolab.com:53798/xecutionerartsblog',
     rootPath: rootPath,
     port: process.env.PORT || 3030
   },
   production: {
     rootPath: rootPath,
     //db: 'mongodb://jeames:multivision@ds053178.mongolab.com:53178/multivision',
-    db: 'mongodb://user:password@ds053798.mongolab.com:53798/xecutionerartsblog',
+    db: 'mongodb://Xecutioner:rbcgnhvf1@ds053798.mongolab.com:53798/xecutionerartsblog',
       port: process.env.PORT || 80
   }
 }
